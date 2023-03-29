3f21f560c418c735cf6a1243a588d7f47e7f2189
Toby Brown <hi@tobyb.dev>
2023-03-29 19:26:08 +0100

    Initial Commit

---
+++ expense-tracker-app/expense_tracker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_tracker_appApp.swift
+//  expense-tracker-app
+//
+//  Created by Toby Brown on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct expense_tracker_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
